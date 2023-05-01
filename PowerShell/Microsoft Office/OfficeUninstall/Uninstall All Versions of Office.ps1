c1a76f5a0e42c0770ec26d8186eb3d6769f335ca
bscheewe <57464630+bscheewe@users.noreply.github.com>
2023-05-01 17:08:47 -0400

    Improvements

--- PowerShell/Microsoft Office/OfficeUninstall/Uninstall All Versions of Office.ps1
+++ PowerShell/Microsoft Office/OfficeUninstall/Uninstall All Versions of Office.ps1
@@ -26,23 +26,21 @@ Set-ExecutionPolicy RemoteSigned -Scope Process -Force
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 $TempFolder = 'C:\temp\UninstallOffice\'
-"Test to see if folder [$TempFolder] exists"
 if (Test-Path -Path $TempFolder) {
     "Temp path exists!"
-} else {
-    "Temp path doesn't exist creating now."
-	mkdir $TempFolder
+}
+else {
+    mkdir $TempFolder
 }
 
 Write-Host "Downloading all required files"
 $BaseURL = "https://github.com/levelsoftware/scripts/raw/main/PowerShell/Microsoft%20Office/OfficeUninstall"
 $O365Setup = "OfficeSetup.exe"
 $ConfigFile = "UninstallOffice.xml"
-$ListOfFiles = $O365Setup,$ConfigFile,"OffScrub03.vbs","OffScrub07.vbs","OffScrub10.vbs","OffScrub_O15msi.vbs","OffScrub_O16msi.vbs","OffScrubc2r.vbs","Office2013Setup.exe","Office2016Setup.exe","Remove-PreviousOfficeInstalls.ps1"
-foreach($File in $ListOfFiles)
-{
+$ListOfFiles = $O365Setup, $ConfigFile, "OffScrub03.vbs", "OffScrub07.vbs", "OffScrub10.vbs", "OffScrub_O15msi.vbs", "OffScrub_O16msi.vbs", "OffScrubc2r.vbs", "Office2013Setup.exe", "Office2016Setup.exe", "Remove-PreviousOfficeInstalls.ps1"
+foreach ($File in $ListOfFiles) {
     iwr -uri $BaseURL$File -outfile $TempFolder$File
-    $FileSize = [math]::round((Get-Item -Path $TempFolder$File).Length/1MB,2)
+    $FileSize = [math]::round((Get-Item -Path $TempFolder$File).Length / 1MB, 2)
     "Downloaded $File - $FileSize MB"
 }
 
@@ -51,16 +49,16 @@ foreach($File in $ListOfFiles)
 Start-Process -Filepath "$TempFolder$O365Setup" -Wait -ArgumentList "/configure $TempFolder$ConfigFile" -PassThru
 
 "Attempting to uninstall any legacy Office installs"
-Start-Process powershell -argument "$TempFolder$File" -Wait -PassThru
+Start-Process powershell -argument $TempFolder"Remove-PreviousOfficeInstalls.ps1" -Wait -PassThru
 
 "Parsing the registry for `"Microsoft 365`" and running the uninstallers"
 $OfficeUninstallStrings = ((Get-ItemProperty "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*") `
-    + (Get-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*") | 
-    Where {$_.DisplayName -like "*Microsoft 365*"} | 
+        + (Get-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*") | 
+    Where { $_.DisplayName -like "*Microsoft 365*" } | 
     Select UninstallString).UninstallString
 
-    ForEach ($UninstallString in $OfficeUninstallStrings) {
-        $UninstallEXE = ($UninstallString -split '"')[1]
-        $UninstallArg = ($UninstallString -split '"')[2] + " DisplayLevel=False"
-        Start-Process -FilePath $UninstallEXE -ArgumentList $UninstallArg -Wait -PassThru
-    }
+ForEach ($UninstallString in $OfficeUninstallStrings) {
+    $UninstallEXE = ($UninstallString -split '"')[1]
+    $UninstallArg = ($UninstallString -split '"')[2] + " DisplayLevel=False"
+    Start-Process -FilePath $UninstallEXE -ArgumentList $UninstallArg -Wait -PassThru
+}
