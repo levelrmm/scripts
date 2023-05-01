1e59fabef3e0b2ac65b343bafd94aed63410f739
bscheewe <57464630+bscheewe@users.noreply.github.com>
2023-05-01 14:12:13 -0400

    Update Winget - Upgrade all apps.ps1

--- PowerShell/Winget/Winget - Upgrade all apps.ps1
+++ PowerShell/Winget/Winget - Upgrade all apps.ps1
@@ -1,3 +1,20 @@
+<#
+This script is provided as a convenience for Level.io customers. We cannot 
+guarantee this will work in all environments. Please test before deploying
+to your production environment.  We welcome contribution to the scripts in 
+our community repo!
+
+.DESCRIPTION
+    Use winget to update all 3rd party apps that winget is capable of 
+    managing.  This will be run both as SYSTEM and as the logged in user.
+.LANGUAGE
+    PowerShell
+.TIMEOUT
+    100
+.LINK
+#>
+
+
 $TestWinget = Get-AppxProvisionedPackage -Online | Where-Object { $_.DisplayName -eq "Microsoft.DesktopAppInstaller" }
 $TestEnvPath = [Environment]::GetEnvironmentVariable("PATH", "Machine") -notlike "*C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe*"
 #Check if Winget is installed
