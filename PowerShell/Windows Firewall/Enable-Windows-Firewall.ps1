0553849ac0883d86470e66a7b878c9d8befd6da7
bscheewe <57464630+bscheewe@users.noreply.github.com>
2023-05-01 14:05:06 -0400

    Creating the script.

---
+++ PowerShell/Windows Firewall/Enable-Windows-Firewall.ps1
@@ -0,0 +1,53 @@
+<#
+This script is provided as a convenience for Level.io customers. We cannot 
+guarantee this will work in all environments. Please test before deploying
+to your production environment.  We welcome contribution to the scripts in 
+our community repo!
+
+.DESCRIPTION
+    Detect the state of the Windows firewall for all profiles, and if the 
+    firewall is disabled, enable it.
+.LANGUAGE
+    PowerShell
+.TIMEOUT
+    100
+.LINK
+#>
+
+#Initialize
+$profileName = $null
+$profileStatus = $null
+$i = 0
+
+#Get the current Windows firewall settings for all profiles (Domain, Private, Public)
+$profileName += get-netfirewallprofile | Select -ExpandProperty name
+$profileStatus += get-netfirewallprofile | Select -ExpandProperty enabled
+
+#List the status of each profile
+foreach ($profileStatusItem in $profileStatus) {
+    if ($profileStatusItem) {
+        Write-host -foregroundcolor green $profileName[$i] "Profile: ENABLED"
+    }
+    else {
+        Write-host -foregroundcolor red $profileName[$i] "Profile: DISABLED" 
+    }
+    $i++
+}
+
+#Enable the Windows firewall for profiles that have it disabled
+if ($profileStatus -contains $true) {
+    Write-Host "Firewall is already enabled on all profiles.  Exiting."
+    exit    
+}
+else {
+    $i = 0
+    foreach ($profileStatusItem in $profileStatus) {
+        if ($profileStatusItem) {
+        }
+        else {
+            Set-NetFirewallProfile -Profile $profileName[$i] -Enabled True
+            Write-host -foregroundcolor green $profileName[$i] "profile firewall has been enabled!" 
+        }
+        $i++
+    }
+}
