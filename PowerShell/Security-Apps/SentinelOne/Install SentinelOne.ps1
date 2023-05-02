810d0f81579522ff3c9c9be4ccf6e3c10a891c9a
bscheewe <57464630+bscheewe@users.noreply.github.com>
2023-05-02 00:29:36 -0400

    SentinelOne Installer

---
+++ PowerShell/Security-Apps/SentinelOne/Install SentinelOne.ps1
@@ -0,0 +1,55 @@
+<#
+This script is provided as a convenience for Level.io customers. We cannot 
+guarantee this will work in all environments. Please test before deploying
+to your production environment.  We welcome contribution to the scripts in 
+our community repo!
+
+.DESCRIPTION
+    Install Sentinel One for Windows 64-bit
+.LANGUAGE
+    PowerShell
+.TIMEOUT
+    100
+.LINK
+#>
+
+#Replace this with your SentinelOne Site Token.  This can be found in your SentinelOne portal under Settings -> Sites.
+$SiteToken = "INSERT_S1_TOKEN_HERE"
+
+#The Windows 64-Bit installer
+$S1_File = "SentinelInstaller_windows_64bit_v22_3_4_612.msi"
+
+#Setup
+Set-ExecutionPolicy RemoteSigned -Scope Process -Force
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+#Check if the SetntinelOne Agent service is already present.
+$servicename = "SentinelAgent"
+if (Get-Service $servicename -ErrorAction SilentlyContinue) {
+    Write-Host "$servicename exists. Exiting..."
+    exit 1
+}
+else {
+    Write-Host "$servicename not found.  Installing Sentinel One."
+}
+
+$TempFolder = 'C:\temp'
+if (Test-Path -Path $TempFolder) {
+}
+else {
+    mkdir c:\temp
+}
+
+#Installer path
+$Installer = "$TempFolder\$S1_File"
+
+#Set VSS size to 10% prior to installing SentinelOne
+vssadmin Resize ShadowStorage /For=C: /On=C: /MaxSize=10%
+
+#Download the installer
+iwr -uri https://github.com/levelsoftware/scripts/raw/main/PowerShell/Security-Apps/SentinelOne/$S1_File -outfile $Installer
+$FileSize = [math]::round((Get-Item -Path $Installer).Length / 1MB, 2)
+"Downloaded $Installer - $FileSize MB"
+
+#Run the installer
+& $Installer /q /norestart UI=false SITE_TOKEN="$SiteToken"
