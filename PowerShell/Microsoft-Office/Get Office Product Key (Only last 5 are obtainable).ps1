5a8ebeb77810402a418d76eacb51f024f32ef1fa
bscheewe <57464630+bscheewe@users.noreply.github.com>
2023-05-04 23:58:41 -0400

    Fixed problem with path array

--- PowerShell/Microsoft-Office/Get Office Product Key (Only last 5 are obtainable).ps1
+++ PowerShell/Microsoft-Office/Get Office Product Key (Only last 5 are obtainable).ps1
@@ -5,21 +5,15 @@ to your production environment.  We welcome contribution to the scripts in
 our community repo!
 
 .DESCRIPTION
-    Pull Office Product Key
+    Only the last 5 characters of an Office key are stored.  This will 
+    go through versions of office and detect which are present and 
+    display the license including the last 5 characters of the key.
 .LANGUAGE
     PowerShell
 .TIMEOUT
     600
 .LINK
 #>
-
-$OfficeKey = (Get-WmiObject -query 'select * from SoftwareLicensingService').OA3xOriginalProductKey
-if ($OfficeKey) {
-    $OfficeKey
-}
-else {
-    wmic path SoftwareLicensingService get OA3xOriginalProductKey
-}
 
 #Office 2016-2021 (32-bit) on a 32-bit Windows OS   "C:\Program Files\Microsoft Office\Office16\OSPP.vbs" 
 #Office 2016-2021 (32-bit) on a 64-bit Windows OS   "C:\Program Files (x86)\Microsoft Office\Office16\OSPP.VBS"
@@ -38,25 +32,26 @@ else {
 #Office 2003 (64-bit) on a 64-bit Windows OS    "C:\Program Files\Microsoft Office\Office11\OSPP.VBS"
 
 $OfficePaths = @"
-"C:\Program Files\Microsoft Office\Office16\OSPP.vbs"
-"C:\Program Files (x86)\Microsoft Office\Office16\OSPP.VBS"
-"C:\Program Files\Microsoft Office\Office16\OSPP.VBS"
-"C:\Program Files\Microsoft Office\Office15\OSPP.VBS"
-"C:\Program Files (x86)\Microsoft Office\Office15\OSPP.vbs"
-"C:\Program Files (x86)\Microsoft Office\Office15\OSPP.VBS"
-"C:\Program Files\Microsoft Office\Office14\OSPP.VBS"
-"C:\Program Files (x86)\Microsoft Office\Office14\OSPP.VBS"
-"C:\Program Files\Microsoft Office\Office14\OSPP.VBS"
-"C:\Program Files\Microsoft Office\Office12\OSPP.VBS"
-"C:\Program Files (x86)\Microsoft Office\Office12\OSPP.VBS"
-"C:\Program Files\Microsoft Office\Office12\OSPP.VBS"
-"C:\Program Files\Microsoft Office\Office11\OSPP.VBS"
-"C:\Program Files (x86)\Microsoft Office\Office11\OSPP.VBS"
-"C:\Program Files\Microsoft Office\Office11\OSPP.VBS"
+C:\Program Files\Microsoft Office\Office16\OSPP.vbs
+C:\Program Files (x86)\Microsoft Office\Office16\OSPP.VBS
+C:\Program Files\Microsoft Office\Office16\OSPP.VBS
+C:\Program Files\Microsoft Office\Office15\OSPP.VBS
+C:\Program Files (x86)\Microsoft Office\Office15\OSPP.vbs
+C:\Program Files (x86)\Microsoft Office\Office15\OSPP.VBS
+C:\Program Files\Microsoft Office\Office14\OSPP.VBS
+C:\Program Files (x86)\Microsoft Office\Office14\OSPP.VBS
+C:\Program Files\Microsoft Office\Office14\OSPP.VBS
+C:\Program Files\Microsoft Office\Office12\OSPP.VBS
+C:\Program Files (x86)\Microsoft Office\Office12\OSPP.VBS
+C:\Program Files\Microsoft Office\Office12\OSPP.VBS
+C:\Program Files\Microsoft Office\Office11\OSPP.VBS
+C:\Program Files (x86)\Microsoft Office\Office11\OSPP.VBS
+C:\Program Files\Microsoft Office\Office11\OSPP.VBS
 "@ -split "`n" | ForEach-Object { $_.trim() }
 
 foreach ($OfficePath in $OfficePaths) {
-    if (Get-Item $OfficePath -ErrorAction SilentlyContinue) {
+    $Present = Get-Item $OfficePath -ErrorAction SilentlyContinue
+    if ($Present) {
         cscript $OfficePath /dstatus
     }
 }
