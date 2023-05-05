ac3d0a5c4ded45791b82e574187e9e539510a32a
bscheewe <57464630+bscheewe@users.noreply.github.com>
2023-05-05 17:37:51 -0400

    Starting AD scripts

---
+++ PowerShell/Active-Directory/AD User Report with last login dates.ps1
@@ -0,0 +1,23 @@
+<#
+This script is provided as a convenience for Level.io customers. We cannot 
+guarantee this will work in all environments. Please test before deploying
+to your production environment.  We welcome contribution to the scripts in 
+our community repo!
+.DESCRIPTION
+    List all enabled users in Active Directory, ordered by last login time. 
+    Useful for finding stale active accounts that should be disabled. If 
+    the last login field is blank it means the account has never logged
+    in.
+
+    Keep in mind that service accounts are only logged in when the service
+    starts.  If a service is running for 90-days straight, then the last
+    login will be 90 days ago.
+
+.LANGUAGE
+    PowerShell
+.TIMEOUT
+    100
+.LINK
+#>
+
+Get-ADUser -Filter {((Enabled -eq $true) -and (LastLogonDate -lt $date))} -Properties LastLogonDate | select samaccountname, Name, LastLogonDate | Sort-Object LastLogonDate
