0f53a13f0ecb9953aa170fbf307991b8c8d6662d
Jacob Haug <jacob@level.io>
2023-06-21 13:47:22 -0400

    adding md5 checks for PS and Bash

---
+++ Bash/Monitors/MD5 Check.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+# This script is provided as a convenience for Level.io customers. We cannot 
+# guarantee this will work in all environments. Please test before deploying
+# to your production environment.  We welcome contribution to the scripts in 
+# our community repo!
+# -----------------------------------------------------------------------------
+#
+# -----------------------------------------------------------------------------
+# Script Configuration
+# -----------------------------------------------------------------------------
+# Name: Linux Monitor - MD5 Check
+# Description: This script compares the MD5 hash of a file with a supplied hash.
+# Language: Bash
+# Timeout: 100
+# Version: 1.0
+#
+# -----------------------------------------------------------------------------
+# Monitor Configuration
+# -----------------------------------------------------------------------------
+# Script: Linux Monitor - MD5 Check
+# Script output: Contains
+# Output value: ERROR
+# Run frequency: Minutes
+# Duration: 5
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# CONFIGURE
+# - file_path
+# - supplied_hash
+
+# File path of the file to check
+file_path="/path/to/file"
+
+# Expected MD5 hash value
+supplied_hash=""
+
+# -----------------------------------------------------------------------------
+
+# Check if the file exists
+if [ ! -f "$file_path" ]; then
+  echo "ERROR: File does not exist: $file_path"
+  exit 1
+fi
+
+# Calculate the MD5 hash of the file
+calculated_hash=$(md5sum "$file_path" | awk '{print $1}')
+
+# Compare the calculated hash with the supplied hash
+if [ "$calculated_hash" == "$supplied_hash" ]; then
+  echo "SUCCESS: The MD5 hash of $file_path matches the supplied hash."
+else
+  echo "ERROR: The MD5 hash of $file_path does not match the supplied hash."
+  echo "Expected hash: $supplied_hash"
+  echo "Calculated hash: $calculated_hash"
+fi
---
+++ PowerShell/Monitoring/MD5 Check.ps1
@@ -0,0 +1,55 @@
+# -----------------------------------------------------------------------------
+# This script is provided as a convenience for Level.io customers. We cannot 
+# guarantee this will work in all environments. Please test before deploying
+# to your production environment.  We welcome contribution to the scripts in 
+# our community repo!
+# -----------------------------------------------------------------------------
+#
+# -----------------------------------------------------------------------------
+# Script Configuration
+# -----------------------------------------------------------------------------
+# Name: Windows Monitor - MD5 Check
+# Description: This script compares the MD5 hash of a file with a supplied hash.
+# Language: PowerShell
+# Timeout: 100
+# Version: 1.0
+#
+# -----------------------------------------------------------------------------
+# Monitor Configuration
+# -----------------------------------------------------------------------------
+# Script: Windows Monitor - MD5 Check
+# Script output: Contains
+# Output value: ERROR
+# Run frequency: Minutes
+# Duration: 5
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# Configuration
+# -----------------------------------------------------------------------------
+$filePath = "C:\path\to\file"
+$suppliedHash = ""
+
+# -----------------------------------------------------------------------------
+
+# Check if the file exists
+if (-not (Test-Path -Path $filePath -PathType Leaf)) {
+    Write-Error "ERROR: File does not exist: $filePath"
+    exit 1
+}
+
+# Calculate the MD5 hash of the file
+$hashAlgorithm = [System.Security.Cryptography.MD5]::Create()
+$hashStream = [System.IO.File]::OpenRead($filePath)
+$calculatedHash = [System.BitConverter]::ToString($hashAlgorithm.ComputeHash($hashStream)).Replace("-", "")
+$hashStream.Close()
+
+# Compare the calculated hash with the supplied hash
+if ($calculatedHash -eq $suppliedHash) {
+    Write-Output "SUCCESS: The MD5 hash of $filePath matches the supplied hash."
+}
+else {
+    Write-Output "ERROR: The MD5 hash of $filePath does not match the supplied hash."
+    Write-Output "Expected hash: $suppliedHash"
+    Write-Output "Calculated hash: $calculatedHash"
+}
