f937cbbaf90318599aef5c87d5fcb06effe23414
bscheewe <57464630+bscheewe@users.noreply.github.com>
2023-06-22 18:20:49 -0400

    Event log monitor adjustments and more comments

--- PowerShell/Monitors/event-log-monitor.ps1
+++ PowerShell/Monitors/event-log-monitor.ps1
@@ -28,25 +28,34 @@
 $LogName = "application"
 #Chose which event ID to monitor.
 $ID = 1000
-#Chose the severity level of the event. (Critical 1, Error 2, Warning 3, Informational 4) Can be comma seperated list (don't use quotes)
+#Chose the severity level of the event. (Critical 1, Error 2, Warning 3, 
+#Informational 4) Can be comma seperated list (don't use quotes)
 $EventSeverity = 2
 #Chose the provider name (source) of the event.
 $ProviderName = "Application Error"
-#Chose the timeframe (in minutes) in which to search.  Search the logs filtered to the past X minutes.
-$Timeframe = 60
+#Chose the timeframe (in minutes) in which to search.  Search the logs filtered 
+#to the past X minutes.  This should be synced up with the monitor run 
+#frequency.  If the frequency will be set to checking every 5 minutes, then the
+#timeframe shouldn't exceed that.
+$Timeframe = 5
+
 
 $TimeSpan = (Get-Date) - (New-TimeSpan -Minutes $Timeframe)
 $ErrorActionPreference = 'silentlycontinue'
-$EventTracker = $null
+
+#Pull the events and filter them
 $EventTracker = Get-WinEvent -FilterHashtable @{
     LogName      = $LogName
     ID           = $ID
     Level        = $EventSeverity
     ProviderName = $ProviderName
     StartTime    = $TimeSpan
-} -MaxEvents 100
+} -MaxEvents 10
+
+#Display the events
 $EventTracker
 
+#If there are events that match, trigger the ALERT
 if ($EventTracker) {
     Write-Output "ALERT"
     Exit 1
