86a0e629dc16c78234a206471ac4c665c40df533
bscheewe <57464630+bscheewe@users.noreply.github.com>
2023-06-22 17:54:52 -0400

    Added event log monitor

--- PowerShell/Monitors/event-log-monitor.ps1
+++ PowerShell/Monitors/event-log-monitor.ps1
@@ -8,9 +8,8 @@
 # -----------------------------------------------------------------------------
 # Script Configuration
 # -----------------------------------------------------------------------------
-# Name: Windows Monitor - Local Disk Errors
-# Description: Checks local disks for errors reported in event viewer within the 
-# last 24 hours
+# Name: Event Log Monitor
+# Description: Scan event log for event IDs and if found trigger an alert
 # Language: PowerShell
 # Timeout: 100
 # Version: 1.0
@@ -18,20 +17,41 @@
 # -----------------------------------------------------------------------------
 # Monitor Configuration
 # -----------------------------------------------------------------------------
-# Script: Windows Monitor - Local Disk Errors
+# Script: Event Log Monitor
 # Script output: Contains
 # Output value: ALERT
 # Run frequency: Minutes
 # Duration: 5
 # -----------------------------------------------------------------------------
 
+#Chose which event log to monitor: application, security, or system
+$LogName = "application"
+#Chose which event ID to monitor.
+$ID = 1000
+#Chose the severity level of the event. (Critical 1, Error 2, Warning 3, Informational 4) Can be comma seperated list (don't use quotes)
+$EventSeverity = 2
+#Chose the provider name (source) of the event.  Can be comma seperated list.
+$ProviderName = "Application Error"
+#Chose the timeframe (in minutes) in which to search.  Search the logs filtered to the past X minutes.
+$Timeframe = 300
+
+$TimeSpan = (Get-Date) - (New-TimeSpan -Minutes $Timeframe)
 $ErrorActionPreference = 'silentlycontinue'
-$TimeSpan = (Get-Date) - (New-TimeSpan -Day 1)
-if (Get-WinEvent -FilterHashtable @{LogName = 'system'; ID = '11', '9', '15', '52', '129', '7', '98'; Level = 2, 3; ProviderName = '*disk*', '*storsvc*', '*ntfs*'; StartTime = $TimeSpan } -MaxEvents 10 | Where-Object -Property Message -Match Volume*) {
+$EventTracker = $null
+$EventTracker = Get-WinEvent -FilterHashtable @{
+    LogName      = $LogName
+    ID           = $ID
+    Level        = $EventSeverity
+    ProviderName = $ProviderName
+    StartTime    = $TimeSpan
+} -MaxEvents 100
+$EventTracker
+
+if ($EventTracker) {
     Write-Output "ALERT"
     Exit 1
 }
 else {
-    Write-Output "Disks are Healthy"
+    Write-Output "Events not found.  Check your filter variables if you are expecting a match."
     Exit 0
 }
